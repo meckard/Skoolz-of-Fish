beb776686d5ef7c7e1fdb89f55353f59f1e14e9b
Micah Eckard <lumberjacksoul339@gmail.com>
2023-11-15 21:24:20 -0500

    added title and photo

--- app/Components/nav.js
+++ app/Components/nav.js
@@ -1,15 +1,18 @@
 import styles from '../page.module.css'
+import Link from 'next/link'
 
 export default function Navbar () {
     return (
         <nav className={styles.navbar}>
-            <p>Skoolz of Fish</p>
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Fish for Sale</li>
-                <li>Media</li>
-            </ul>
+            <div className={styles.navElements}>
+                <p>SoF</p>
+                <ul>
+                    <Link href='/'><li>Home</li></Link>
+                    <Link href='/about'><li>About Us</li></Link>
+                    <Link href='/fish'><li>Fish for Sale</li></Link>
+                    <Link href='/media'><li>Media</li></Link>
+                </ul>
+            </div>
         </nav>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,20 +1,18 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import Image from 'next/image'
+import tetrapainting from './Resources/Images/Tetra-Oil-Painting.png'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.fishtank}>
-        <div className={styles.tank}>
-          <div className={styles.water}>
-            <div className={styles.fish1}>
-              <div className={styles.fishBody}></div>
-              <div className={styles.fishFin1}></div>
-              <div className={styles.fishFin2}></div>
-              <div className={styles.fishEye}></div>
-            </div>
-          </div>
-        </div>
+      <div className={styles.title}>
+        <Image src={tetrapainting}
+        width={500}
+        height={500}
+        className={styles.tetra}
+        alt='an oil painting of a neon tetra'/>
+        <h1>Skoolz of Fish</h1>
+        <h2>Richmond's premiere discount freshwater fish and supplies</h2>
       </div>
     </main>
   )
