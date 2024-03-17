e6a77005dceca8d07540491a625f326b43cb62e2
Micah Eckard <lumberjacksoul339@gmail.com>
2024-03-16 20:45:18 -0400

    Never needed to hide it

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,12 +1,83 @@
-import styles from "../page.module.css";
-import ContactForm from "../Components/contactForm";
+"use client";
 
-export default function Contact() {
-	const AK = process.env.CONTACT_FORM;
+import styles from "../page.module.css";
+
+export default async function Contact() {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
+
+	const nameField = (e) => {
+		setName(e.target.value);
+	};
+
+	const emailField = (e) => {
+		setEmail(e.target.value);
+	};
+
+	const messageField = (e) => {
+		setMessage(e.target.value);
+	};
+
+	async function handleSubmit(event) {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+
+		formData.append("access_key", "7f2bf601-e7fe-41b5-bbb1-5ab90bb5e478");
+
+		const object = Object.fromEntries(formData);
+		const json = JSON.stringify(object);
+
+		const response = await fetch("https://api.web3forms.com/submit", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+			},
+			body: json,
+		});
+		const result = await response.json();
+		if (result.success) {
+			console.log(result);
+			setName("");
+			setEmail("");
+			setMessage("");
+		}
+	}
 
 	return (
 		<div className={styles.contactPage}>
-			<ContactForm key={AK} />
+			<div className={styles.contactUs}>
+				<h2>Contact Us</h2>
+				<>
+					<form
+						className={styles.contactForm}
+						onSubmit={handleSubmit}
+					>
+						<input
+							type="text"
+							onChange={nameField}
+							value={name}
+							placeholder="Name"
+							name="name"
+						/>
+						<input
+							type="email"
+							onChange={emailField}
+							value={email}
+							placeholder="Email"
+							name="email"
+						/>
+						<textarea
+							placeholder="Message Here"
+							onChange={messageField}
+							value={message}
+							name="message"
+						></textarea>
+						<button type="submit">Submit Form</button>
+					</form>
+				</>
+			</div>
 			<div className={styles.storeInfo}>
 				<h2>Store Info</h2>
 				<ul className={styles.hours}>
